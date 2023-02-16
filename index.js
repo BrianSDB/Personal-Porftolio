bad429e0a45d27a2b91da2ad7676f7fca010b369
Brian <118107819+BrianSDB@users.noreply.github.com>
2023-02-16 16:00:25 +0000

    linked with java

---
+++ index.js
@@ -0,0 +1,26 @@
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile = document.querySelector('.header .nav-bar .nav-list ul');
+const menuItem = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+
+hamburger.addEventListener('click',()=>{
+    hamburger.classList.toggle('active');
+    mobile.classList.toggle('active');
+})
+
+document.addEventListener('scroll',()=>{
+    var scroll_pos = window.scrollY;
+    if(scroll_pos > 250){
+        header.style.backgroundColor = '#191970';
+    } else {
+        header.style.backgroundColor = 'transparent';
+
+    }
+
+})
+
+menuItem.forEach(item=>{
+    item.addEventListener('click', ()=>{
+        mobile.classList.toggle('active');
+    })
+})
